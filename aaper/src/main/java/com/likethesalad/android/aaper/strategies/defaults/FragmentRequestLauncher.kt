2faaa8bd0f5f3aa2bfbf4a8c4a5cbc37197ba569
Cesar <amqcesar@gmail.com>
2020-08-03 12:49:38 +0100

    Created FragmentRequestLauncher

---
+++ aaper/src/main/java/com/likethesalad/android/aaper/strategies/defaults/FragmentRequestLauncher.kt
@@ -0,0 +1,18 @@
+package com.likethesalad.android.aaper.strategies.defaults
+
+import androidx.fragment.app.Fragment
+import com.likethesalad.android.aaper.api.base.RequestLauncher
+
+/**
+ * Created by César Muñoz on 03/08/20.
+ */
+class FragmentRequestLauncher : RequestLauncher<Fragment>() {
+
+    override fun launchPermissionsRequest(
+        host: Fragment,
+        permissions: List<String>,
+        requestCode: Int
+    ) {
+        host.requestPermissions(permissions.toTypedArray(), requestCode)
+    }
+}
