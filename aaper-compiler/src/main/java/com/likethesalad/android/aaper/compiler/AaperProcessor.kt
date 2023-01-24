2db6d4dddf8dfd1f1405dc4771dcdf63d6a45324
Cesar Munoz <likethesalad@gmail.com>
2023-01-24 21:07:49 +0100

    Adding config params to processor

--- aaper-compiler/src/main/java/com/likethesalad/android/aaper/compiler/AaperProcessor.kt
+++ aaper-compiler/src/main/java/com/likethesalad/android/aaper/compiler/AaperProcessor.kt
@@ -2,14 +2,19 @@ package com.likethesalad.android.aaper.compiler
 
 import javax.annotation.processing.AbstractProcessor
 import javax.annotation.processing.RoundEnvironment
+import javax.annotation.processing.SupportedAnnotationTypes
+import javax.annotation.processing.SupportedSourceVersion
+import javax.lang.model.SourceVersion
 import javax.lang.model.element.TypeElement
 
+@SupportedAnnotationTypes("com.likethesalad.android.aaper.api.EnsurePermissions")
+@SupportedSourceVersion(SourceVersion.RELEASE_8)
 class AaperProcessor : AbstractProcessor() {
 
     override fun process(
         annotations: MutableSet<out TypeElement>,
         roundEnv: RoundEnvironment
     ): Boolean {
-        TODO("Not yet implemented")
+        return true
     }
 }
