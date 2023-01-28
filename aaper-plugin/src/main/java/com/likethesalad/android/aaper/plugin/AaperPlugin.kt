47db99d16332e0a84ac64046114123e69a6bfe4c
Cesar Munoz <likethesalad@gmail.com>
2023-01-28 16:11:09 +0100

    Setting up aaper gradle plugin

---
+++ aaper-plugin/src/main/java/com/likethesalad/android/aaper/plugin/AaperPlugin.kt
@@ -0,0 +1,11 @@
+package com.likethesalad.android.aaper.plugin
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+class AaperPlugin : Plugin<Project> {
+
+    override fun apply(project: Project) {
+        TODO("Not yet implemented")
+    }
+}
