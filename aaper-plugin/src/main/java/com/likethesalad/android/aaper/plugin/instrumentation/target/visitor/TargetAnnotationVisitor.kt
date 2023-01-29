e3ab69ac0b1a035fa1090f65c7f62d933bee1304
Cesar Munoz <likethesalad@gmail.com>
2023-01-29 16:49:13 +0100

    Capturing permissions list

--- aaper-plugin/src/main/java/com/likethesalad/android/aaper/plugin/instrumentation/target/visitor/TargetAnnotationVisitor.kt
+++ aaper-plugin/src/main/java/com/likethesalad/android/aaper/plugin/instrumentation/target/visitor/TargetAnnotationVisitor.kt
@@ -5,17 +5,27 @@ import org.objectweb.asm.Opcodes
 
 class TargetAnnotationVisitor(annotationVisitor: AnnotationVisitor) :
     AnnotationVisitor(Opcodes.ASM9, annotationVisitor) {
+    private var permissionsAnnotationVisitor: PermissionsAnnotationVisitor? = null
     var strategyName: String? = null
         private set
 
-    override fun visit(name: String?, value: Any?) {
+    override fun visit(name: String, value: Any?) {
         super.visit(name, value)
         if (name == "strategyName") {
             strategyName = value?.toString()
         }
     }
 
-    override fun visitArray(name: String?): AnnotationVisitor {
-        return super.visitArray(name)
+    override fun visitArray(name: String): AnnotationVisitor {
+        val visitor = super.visitArray(name)
+        if (name == "permissions") {
+            permissionsAnnotationVisitor = PermissionsAnnotationVisitor(visitor)
+            return permissionsAnnotationVisitor!!
+        }
+        return visitor
+    }
+
+    fun getPermissions(): List<String> {
+        return permissionsAnnotationVisitor?.permissions ?: emptyList()
     }
 }
