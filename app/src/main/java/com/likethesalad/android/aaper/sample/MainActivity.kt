ece291f202a95102dbfcac2aa1ad5ff938bb39c3
Cesar Munoz <likethesalad@gmail.com>
2023-02-02 19:21:49 +0100

    Imports formatting

--- app/src/main/java/com/likethesalad/android/aaper/sample/MainActivity.kt
+++ app/src/main/java/com/likethesalad/android/aaper/sample/MainActivity.kt
@@ -6,7 +6,6 @@ import android.widget.Button
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.likethesalad.android.aaper.api.EnsurePermissions
-
 import com.likethesalad.android.aaper.sample.custom.AlertDialogStrategy
 
 class MainActivity : AppCompatActivity() {
