3c535444017e418d5050a832f0b150e1b80d128f
Cesar Munoz <likethesalad@gmail.com>
2023-02-03 10:57:17 +0100

    Adding test app to base robolectric test

--- app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/BaseRobolectricTest.kt
+++ app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/BaseRobolectricTest.kt
@@ -2,6 +2,8 @@ package com.likethesalad.android.aaper.sample.test.testutils
 
 import org.junit.runner.RunWith
 import org.robolectric.RobolectricTestRunner
+import org.robolectric.annotation.Config
 
 @RunWith(RobolectricTestRunner::class)
+@Config(application = TestApplication::class)
 abstract class BaseRobolectricTest
