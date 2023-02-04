199f3b2caf54a5f4cb0beba1c8fef632f9033b1f
Cesar Munoz <likethesalad@gmail.com>
2023-02-04 16:56:48 +0100

    Updated sample app

--- app/src/main/java/com/likethesalad/android/aaper/sample/MyApp.kt
+++ app/src/main/java/com/likethesalad/android/aaper/sample/MyApp.kt
@@ -1,6 +1,9 @@
 package com.likethesalad.android.aaper.sample
 
 import android.app.Application
+import com.likethesalad.android.aaper.Aaper
+import com.likethesalad.android.aaper.defaults.DefaultRequestStrategyProvider
+import com.likethesalad.android.aaper.sample.custom.AlertDialogStrategy
 
 /**
  * Created by César Muñoz on 03/08/20.
@@ -9,10 +12,10 @@ class MyApp : Application() {
 
     override fun onCreate() {
         super.onCreate()
-//        Aaper.init()
+        Aaper.init()
 
-//        val strategyProvider = Aaper.getRequestStrategyProvider() as DefaultRequestStrategyProvider
-//        strategyProvider.register(AlertDialogStrategy())
+        val strategyProvider = Aaper.getRequestStrategyProvider() as DefaultRequestStrategyProvider
+        strategyProvider.register(AlertDialogStrategy())
 //        strategyProvider.register(FinishActivityOnDeniedStrategy())
 //        strategyProvider.setDefaultStrategyName("FinishActivityOnDenied")
     }
