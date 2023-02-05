1844baf01f83ff416bf664156ea94ac9f37cc4a5
Cesar Munoz <likethesalad@gmail.com>
2023-02-05 12:34:59 +0100

    Creating the Aaper.setUp method as replacement for init

--- aaper/src/main/java/com/likethesalad/android/aaper/Aaper.kt
+++ aaper/src/main/java/com/likethesalad/android/aaper/Aaper.kt
@@ -1,9 +1,9 @@
 package com.likethesalad.android.aaper
 
+import android.content.Context
 import com.likethesalad.android.aaper.api.PermissionManager
 import com.likethesalad.android.aaper.api.base.RequestStrategyProvider
 import com.likethesalad.android.aaper.defaults.DefaultRequestStrategyProvider
-import com.likethesalad.android.aaper.defaults.strategies.DefaultRequestStrategy
 import com.likethesalad.android.aaper.errors.AaperInitializedAlreadyException
 import com.likethesalad.android.aaper.internal.base.RequestStrategyProviderSource
 
@@ -31,24 +31,23 @@ object Aaper : RequestStrategyProviderSource {
     }
 
     /**
-     * Sets Aaper's [RequestStrategyProvider] which is needed to handle permission requests later on.
+     * Initializes Aaper
      * This method is supposed to be called automatically so there's no need for it to be called,
-     * unless you wanted to override the default provider.
+     * unless you wanted to override the default behavior, should it be the case, you'd need to
+     * disable its automatic initialization first. More info on the README.
+     *
+     * @param context - The app context
+     * @param strategyProvider - Will delegate permission requests to its strategies.
      */
-    fun setRequestStrategyProvider(strategyProvider: RequestStrategyProvider) {
+    fun setUp(context: Context, strategyProvider: RequestStrategyProvider) = synchronized(this) {
         if (initialized) {
             throw AaperInitializedAlreadyException()
         }
 
-        initialized = true
-
         this.strategyProvider = strategyProvider
         PermissionManager.setStrategyProviderSource(this)
 
-        if (strategyProvider is DefaultRequestStrategyProvider) {
-            strategyProvider.register(DefaultRequestStrategy())
-            strategyProvider.setDefaultStrategyName(DefaultRequestStrategy.NAME)
-        }
+        initialized = true
     }
 
     /**
