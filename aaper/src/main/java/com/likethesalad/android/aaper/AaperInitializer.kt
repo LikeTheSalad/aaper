92a5947fb315080e36fe6e22c8e7102eda882ce1
Cesar Munoz <likethesalad@gmail.com>
2023-02-05 12:37:10 +0100

    Initializing Aaper using the new method

--- aaper/src/main/java/com/likethesalad/android/aaper/AaperInitializer.kt
+++ aaper/src/main/java/com/likethesalad/android/aaper/AaperInitializer.kt
@@ -2,14 +2,20 @@ package com.likethesalad.android.aaper
 
 import android.content.Context
 import androidx.startup.Initializer
+import com.likethesalad.android.aaper.defaults.DefaultRequestStrategyProvider
+import com.likethesalad.android.aaper.defaults.strategies.DefaultRequestStrategy
 
 class AaperInitializer : Initializer<Unit> {
 
-  override fun create(context: Context) {
-    Aaper.init()
-  }
+    override fun create(context: Context) {
+        val strategyProvider = DefaultRequestStrategyProvider()
+        strategyProvider.register(DefaultRequestStrategy())
+        strategyProvider.setDefaultStrategyName(DefaultRequestStrategy.NAME)
 
-  override fun dependencies(): List<Class<out Initializer<*>>> {
-    return emptyList()
-  }
+        Aaper.setUp(context, strategyProvider)
+    }
+
+    override fun dependencies(): List<Class<out Initializer<*>>> {
+        return emptyList()
+    }
 }
