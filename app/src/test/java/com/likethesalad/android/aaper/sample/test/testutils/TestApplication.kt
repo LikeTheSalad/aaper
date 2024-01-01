197fedc0343251b5bab8ad57b5b9efa236284fdf
Cesar Munoz <likethesalad@gmail.com>
2024-01-01 14:59:11 +0100

    Fixing instrumentation tests

--- app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/TestApplication.kt
+++ app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/TestApplication.kt
@@ -2,11 +2,13 @@ package com.likethesalad.android.aaper.sample.test.testutils
 
 import android.app.Application
 import com.likethesalad.android.aaper.Aaper
+import com.likethesalad.android.aaper.sample.test.testutils.strategies.AlwaysSuccessfulStrategy
 
 class TestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
         Aaper.initialize(this)
+        Aaper.setDefaultStrategyType(AlwaysSuccessfulStrategy::class.java)
     }
 }
