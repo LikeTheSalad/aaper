96365ba61dfbae84a0a055e21b9c2f024e4894ba
Cesar Munoz <likethesalad@gmail.com>
2024-01-01 09:48:19 +0100

    Clean up

--- aaper/src/main/java/com/likethesalad/android/aaper/Aaper.kt
+++ aaper/src/main/java/com/likethesalad/android/aaper/Aaper.kt
@@ -16,21 +16,6 @@ object Aaper : RequestStrategyProviderSource {
     private var initialized = false
 
     /**
-     * Initializes Aaper, this function must be called once only and it must be done
-     * before any permission request. Therefore, a great place to call it is the
-     * "Application.onCreate" function.
-     *
-     * @param strategyProvider - (Optional) If you want to override the default [RequestStrategyProvider]
-     * instance by a custom one, you can pass yours here. Otherwise, the default will be
-     * [DefaultRequestStrategyProvider].
-     */
-    @Deprecated(message = "Deprecated after adding automatic initialization support on version 2.1.0, there's no longer need to manually call this method.")
-    @JvmOverloads
-    fun init(strategyProvider: RequestStrategyProvider = DefaultRequestStrategyProvider()) {
-        // No operation
-    }
-
-    /**
      * Initializes Aaper
      * This method is supposed to be called automatically so there's no need for it to be called,
      * unless you wanted to override the default behavior, should it be the case, you'd need to
@@ -52,7 +37,7 @@ object Aaper : RequestStrategyProviderSource {
 
     /**
      * Returns the instance of [RequestStrategyProvider] being used, which is the one set on
-     * the [init] function. If no custom instance was passed, then this function will return a
+     * the [setUp] function. If no custom instance was passed, then this function will return a
      * [DefaultRequestStrategyProvider] one.
      */
     @Suppress("UNCHECKED_CAST")
