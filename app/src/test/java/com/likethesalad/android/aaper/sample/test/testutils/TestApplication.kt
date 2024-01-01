969eb41f8483eb176ea278bba31d618eb3da5705
Cesar Munoz <likethesalad@gmail.com>
2024-01-01 13:13:39 +0100

    Adapting sample app to new api

--- app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/TestApplication.kt
+++ app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/TestApplication.kt
@@ -2,17 +2,11 @@ package com.likethesalad.android.aaper.sample.test.testutils
 
 import android.app.Application
 import com.likethesalad.android.aaper.Aaper
-import com.likethesalad.android.aaper.defaults.DefaultRequestStrategyProvider
-import com.likethesalad.android.aaper.sample.test.testutils.strategies.AlwaysSuccessfulStrategy
 
 class TestApplication : Application() {
 
     override fun onCreate() {
         super.onCreate()
-        val strategyProvider = DefaultRequestStrategyProvider()
-        Aaper.setUp(this, strategyProvider)
-
-        strategyProvider.register(AlwaysSuccessfulStrategy())
-        strategyProvider.setDefaultStrategyName(AlwaysSuccessfulStrategy.NAME)
+        Aaper.initialize(this)
     }
 }
--- app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/strategies/AlwaysSuccessfulStrategy.kt
+++ app/src/test/java/com/likethesalad/android/aaper/sample/test/testutils/strategies/AlwaysSuccessfulStrategy.kt
@@ -1,15 +1,11 @@
 package com.likethesalad.android.aaper.sample.test.testutils.strategies
 
-import com.likethesalad.android.aaper.api.base.RequestLauncher
 import com.likethesalad.android.aaper.api.data.PermissionsResult
-import com.likethesalad.android.aaper.base.strategies.impl.AllRequestStrategy
+import com.likethesalad.android.aaper.api.launcher.RequestLauncher
+import com.likethesalad.android.aaper.base.common.strategy.AllRequestStrategy
 import com.likethesalad.android.aaper.sample.test.testutils.launcher.TestRequestLauncher
 
 class AlwaysSuccessfulStrategy : AllRequestStrategy() {
-
-    companion object {
-        const val NAME = "all-success"
-    }
 
     override fun onPermissionsRequestResults(host: Any, data: PermissionsResult): Boolean {
         return true
@@ -17,9 +13,5 @@ class AlwaysSuccessfulStrategy : AllRequestStrategy() {
 
     override fun getRequestLauncher(host: Any): RequestLauncher<Any> {
         return TestRequestLauncher()
-    }
-
-    override fun getName(): String {
-        return NAME
     }
 }
